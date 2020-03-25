b790957dad85d544e2e9b2056d9bc0b2458e7729
Juho Taskila <juho@jtaskila.fi>
2020-03-25 13:51:38 +0200

    HTTP abstraction finished
    Finished requests are now processed with a lambda functions, so id can be passed through. This id can be used to identify requests when handling them in the DataController.
    (also some memory leaks fixed)

--- client/api/JsonAPI/jsonrequestcontroller.cpp
+++ client/api/JsonAPI/jsonrequestcontroller.cpp
@@ -1,15 +1,11 @@
 #include "jsonrequestcontroller.h"
 
-JsonRequestController::JsonRequestController(QObject *parent) : QObject(parent)
-{
-    this->requestManager = new QNetworkAccessManager(this);
-}
+JsonRequestController::JsonRequestController(QObject *parent) : QObject(parent){}
 
-JsonRequestController::~JsonRequestController(){
-    delete this->requestManager;
-}
+JsonRequestController::~JsonRequestController(){}
 
-void JsonRequestController::request(int type, QString endPoint, QString apiKey, QString params){
+void JsonRequestController::request(int id, int type, QString endPoint, QString apiKey, QString params){
+    QNetworkAccessManager *requestManager = new QNetworkAccessManager(this);
     QNetworkRequest request;
     QNetworkReply *response;
 
@@ -24,20 +20,31 @@ void JsonRequestController::request(int type, QString endPoint, QString apiKey, 
     if(type == 1){
         request.setUrl(QUrl(endPoint + QString("?") + params));
 
-        response = this->requestManager->get(request);
+        response = requestManager->get(request);
 
         qDebug() << "GET request sent, waiting for response" << endl;
 
-        QObject::connect(this->requestManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(processRequest(QNetworkReply*)));
+        // using lambda to pass the request id forward
+        QObject::connect(requestManager, &QNetworkAccessManager::finished, [this, response, id, requestManager](){
+            this->processRequest(id, response);
+            delete requestManager;
+            delete response;
+        });
 
     }else if(type == 2){
         request.setUrl(QUrl(endPoint));
 
-        response = this->requestManager->post(request, QByteArray(params.toUtf8()));
+        response = requestManager->post(request, QByteArray(params.toUtf8()));
 
         qDebug() << "POST request sent, waiting for response" << endl;
 
-        QObject::connect(this->requestManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(processRequest(QNetworkReply*)));
+        // using lambda to pass the request id forward
+        QObject::connect(requestManager, &QNetworkAccessManager::finished, [this, response, id, requestManager](){
+            this->processRequest(id, response);
+            delete requestManager;
+            delete response;
+        });
+
     }else{
         emit requestError(101, "Invalid request type");
         return;
@@ -45,14 +52,18 @@ void JsonRequestController::request(int type, QString endPoint, QString apiKey, 
 
 }
 
+void JsonRequestController::processRequest(int id, QNetworkReply *response){
+    QString responsePlain = response->readAll();
 
-void JsonRequestController::processRequest(QNetworkReply *response){
     if(response->error()){
-        emit requestError(102, "Error in HTTP request");
         qDebug() << response->errorString() << endl;
+
+        emit requestError(102, "Error in HTTP request");        
         return;
     }
 
-    qDebug() << response->readAll() << endl;
-    emit requestComplete(response->readAll());
+    qDebug() << "Request id: " << id << endl;
+    qDebug() << responsePlain << endl;
+
+    emit requestComplete(id, responsePlain);
 }
--- client/api/JsonAPI/jsonrequestcontroller.h
+++ client/api/JsonAPI/jsonrequestcontroller.h
@@ -5,7 +5,6 @@
 #include <QtNetwork/QNetworkAccessManager>
 #include <QtNetwork/QNetworkRequest>
 #include <QtNetwork/QNetworkReply>
-
 #include <QDebug>
 
 class JsonRequestController : public QObject
@@ -16,17 +15,14 @@ public:
     explicit JsonRequestController(QObject *parent = 0);
     ~JsonRequestController();
 
-    void request(int type, QString endPoint, QString apiKey, QString params);
-
-private:
-    QNetworkAccessManager *requestManager;
+    void request(int id, int type, QString endPoint, QString apiKey, QString params);
 
 private slots:
-    void processRequest(QNetworkReply* response);
+    void processRequest(int id, QNetworkReply* response);
 
 signals:
     void requestError(int code, QString msg);
-    void requestComplete(QString response);
+    void requestComplete(int id, QString response);
 };
 
 #endif // JSONREQUESTCONTROLLER_H
