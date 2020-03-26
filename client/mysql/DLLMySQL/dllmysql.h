26bc3d8bc3e5ab4f5377dddb520ebf8719de50d6
Juri Haataja <39046851+JuriH@users.noreply.github.com>
2020-03-26 15:35:21 +0200

    DLLMySQL: Added Login & Withdraw functions
    Added Login function which checks for corresponding data from the database and Withdraw that first checks the corresponding ID's balance and then checks if the amount to be withdrawn is equal or less than the total balance. If true, does the math and updates the total balance to its new value.

--- client/mysql/DLLMySQL/dllmysql.h
+++ client/mysql/DLLMySQL/dllmysql.h
@@ -5,17 +5,35 @@
 #include <QDebug>
 #include <QSqlDatabase>
 #include <QtSql>
-#include <QCoreApplication>
+#include <QString>
+#include <QSqlQuery>
+
 
 class DLLMySQL
 {
 public:
+    struct PaymentInfo {
+        int type;
+        double amount;
+        QString name;
+        QString iban;
+        QString bicc;
+        QString message;
+        QString reference;
+    };
     static QSqlDatabase& connectToDb();
+    static QSqlDatabase& login(int cardid, QString pin);
+    //static bool login(int cardid, int pin);
+    static QSqlDatabase& withdraw(int id, double amount);
+    void payment(double amount, PaymentInfo info);
+    void events(QDate dateStart, QDate dateEnd);
+    double balance();
+    bool makeQuery();
 };
 
 QSqlDatabase& DLLMySQL::connectToDb()
 {
-    static QSqlDatabase db;
+    QSqlDatabase db;
     db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("mysli.oamk.fi");     // host
     db.setDatabaseName("opisk_t9haju03");            // database
@@ -23,16 +41,76 @@ QSqlDatabase& DLLMySQL::connectToDb()
     db.setUserName("t9haju03");                // user
     db.setPassword("a6n6QLe8pEC7nJ4j");              // password
 
-    bool ok = db.open();
-
-    if(ok){
+    if(db.open()){
         qDebug() << "Connection to database open";
     } else {
         qDebug() << "Driver in use: " + db.driverName();
         qDebug() << "Couldn't open connection to database";
         qDebug() << db.lastError().text();
     }
-    return db;
 }
+
+QSqlDatabase& DLLMySQL::login(int cardid, QString pin)
+{
+    if (QSqlDatabase::contains()){
+        QSqlDatabase db = QSqlDatabase::database("MyDbConnection");
+        QSqlQuery query;
+        query.prepare("SELECT user_id, user_pin FROM kirjautuminen WHERE user_id = ? AND user_pin = ?");
+        query.addBindValue(cardid);
+        query.addBindValue(pin);
+        query.exec();
+        if (query.size() > 0){
+            qDebug() << "User found";
+        } else {
+            qDebug() << "User not found";
+        }
+    } else {
+        QSqlDatabase db;
+        db = QSqlDatabase::addDatabase("QMYSQL");
+        db.setHostName("mysli.oamk.fi");     // host
+        db.setDatabaseName("opisk_t9haju03");            // database
+        db.setPort(3306);
+        db.setUserName("t9haju03");                // user
+        db.setPassword("a6n6QLe8pEC7nJ4j");
+    }
+}
+
+QSqlDatabase& DLLMySQL::withdraw(int cardid, double amount)
+{
+    if (QSqlDatabase::contains()){
+        QSqlDatabase db = QSqlDatabase::database("MyDbConnection");
+        QSqlQuery query;
+        query.prepare("SELECT saldo FROM tilitiedot WHERE user_id = ?");
+        query.addBindValue(cardid);
+        query.exec();
+        query.first();
+        if (query.size() >= 0){
+            qDebug() << "Found user's balance";
+            double balance = query.value(0).toDouble();
+            qDebug() << "User with ID: " << cardid << "'s balance is " << balance << " euro(s)";
+            if(amount > balance){
+                qDebug() << "Not enough funds (" << balance << " euro(s)) to complete the withdraw of " << amount << " euro(s)";
+            } else if (amount <= balance){
+                double math = balance - amount;
+                query.prepare("UPDATE tilitiedot SET saldo = ? WHERE user_id = ?");
+                query.addBindValue(math);
+                query.addBindValue(cardid);
+                if(query.exec()){
+                    qDebug() << "Withdraw completed successfully";
+                } else {
+                    qDebug() << "Withdraw failed";
+                }
+            }
+        } else if (query.size() == 0){
+            qDebug() << "Didn't find user's balance";
+        }
+    } else {
+        qDebug() << "Couldn't connect to database";
+    }
+}
+
+
+
+
 
 #endif // DLLMYSQL_H
