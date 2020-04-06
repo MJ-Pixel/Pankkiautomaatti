d7a3995684ebc4fa3d6b46e1c8313a679aeec2d3
MJ-Pixel <59865389+MJ-Pixel@users.noreply.github.com>
2020-04-06 07:44:07 +0300

    rfid

---
+++ client/rfid/DLLSarjaPortti/dllsarjaportti.h
@@ -0,0 +1,28 @@
+#ifndef DLLSARJAPORTTI_H
+#define DLLSARJAPORTTI_H
+#include <QSerialPort>
+#include <QDebug>
+#include <QString>
+#include <QObject>
+#include "dllsarjaportti_global.h"
+
+using namespace std;
+
+class DLLSARJAPORTTISHARED_EXPORT DLLSarjaPortti : public QObject
+{
+    Q_OBJECT
+public:
+   // DLLSarjaPortti();
+    void DLLSARJAPORTTISHARED_EXPORT readCard();
+private:
+    QString cardID;
+    QString returnCardID;
+    QSerialPort *serial;
+signals:
+    void DLLSARJAPORTTISHARED_EXPORT aSignal();
+public slots:
+    QString writeData();
+    void DLLSARJAPORTTISHARED_EXPORT bSignal();
+};
+
+#endif // DLLSARJAPORTTI_H
